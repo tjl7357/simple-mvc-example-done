1b4ffd078defad3b565dc961b55abf750d36eb0f
TJ Larson <tjl7357@rit.edu>
2023-03-23 19:34:43 -0400

    Finished first page

---
+++ server/models/Dog.js
@@ -0,0 +1,38 @@
+// Requires
+const mongoose = require('mongoose');
+
+//  Declare Dog Model
+let DogModel = {};
+
+// Defining Dog Schema
+const DogSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+    },
+
+    breed: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+
+    age: {
+        type: Number,
+        min: 0,
+        required: true,
+    },
+
+    createdDate: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+// Connect the Schema to the Model
+DogModel = mongoose.model('Dog', DogSchema);
+
+// Export the model
+module.exports = DogModel;
